6a15701b6e2050b792a0b33e97b7d6527f9fe56e
bfazeli <bfazeli1@gmail.com>
2019-03-01 16:01:16 -0800

    Update: Implemented qList where qList generates a list of QuadPaths

--- HeathrowToLondon/heathrow_to_london.hs
+++ HeathrowToLondon/heathrow_to_london.hs
@@ -1,10 +1,14 @@
 import Debug.Trace
 
 -- while loop
-while:: state -> (state -> Bool) -> (state -> state) -> (state -> result) -> result
+while:: ([Int], [QuadPaths]) -> (([Int], [QuadPaths]) -> Bool) -> (([Int], [QuadPaths]) -> ([Int], [QuadPaths])) -> (([Int], [QuadPaths]) -> result) -> result
 while state shouldContinue action finalAction =
-    if shouldContinue state then while (action state) shouldContinue action finalAction
-    else finalAction state
+    if shouldContinue state then 
+        -- trace("The value of state is: " ++ show state) 
+        while (action state) shouldContinue action finalAction
+    else 
+        trace("The final state is: " ++ show state)
+        finalAction state
 
 -- A roadsystem consists of N amount of sections, where each section consists of K amount of steps
 data Section = Section { getA :: Int, getB :: Int, getC:: Int } deriving (Show)
@@ -26,7 +30,7 @@ newtype QuadPaths = QuadPaths [Path] deriving (Show)
 
 -- Handling display of custom types
 instance Show Path where
-    show (Path (shortestSteps, distance, possibleSteps)) = show shortestSteps ++ "." ++ "Dist: " ++ show distance ++ "." ++ show possibleSteps
+    show (Path (shortestSteps, distance, possibleSteps)) = "\n" ++ show shortestSteps ++ "." ++ show distance ++ "." ++ show possibleSteps
 instance Show ShortestStep where
     show (ShortestStep (a, b)) = show a ++ "->" ++ show b
 instance Show Step where
@@ -37,28 +41,64 @@ instance Num Distance where
     Distance a + Distance b = Distance(a + b)
     -- p@(Path steps)
 
-optimalPaths:: Int -> Int -> Distance
-optimalPaths input =
-    let result = optimalPaths' $ trace("\noptimalPaths (input):\n" ++ show input) input
-    in trace ("\noptimalPaths (output): " ++ show result ++ "\n") result
+-- optimalPaths:: Int -> Int -> Distance
+-- optimalPaths input =
+--     let result = optimalPaths' $ trace("\noptimalPaths (input):\n" ++ show input) input
+--     in trace ("\noptimalPaths (output): " ++ show result ++ "\n") result
 
-optimalPaths' input = sort . (\(QuadPaths paths) -> paths) . combinePaths . qpList
+-- optimalPaths' input = sort . (\(QuadPaths paths) -> paths) . combinePaths . qpList
 
-combinePaths :: [QuadPaths] -> QuadPaths
-combinePaths (qp : qps) = foldl join qp qps
+-- combinePaths :: [QuadPaths] -> QuadPaths
+-- combinePaths (qp : qps) = foldl join qp qps
 
 -- qpList will return an array of QuadPaths
 qpList :: [Int] -> [QuadPaths]
-qpList input = 
-    while 
-        (input, [])
-        (not . null (tail(tail . fst)))
-        (\(input, output) -> (drop 3 input, output + createQuad (take 3 input)))
-        (\(input, output) -> (output + createQuad (take 2 input)))
+qpList input =
+    -- | length input - 2 `mod` 3 /= 0 =
+    --     [QuadPaths]
+    -- | otherwise = 
+        while 
+            (input, [])
+            (\(input, output) -> (not (null input)))
+            (\(input, output) -> (drop 3 input, createQuad (take 3 input) : output))
+            (reverse . snd)
 
 -- TODO: Implement createQuad
+createQuad :: [Int] -> QuadPaths
+createQuad input
+    | length input >= 3 = 
+        QuadPaths [
+            Path (ShortestStep(A, A), Distance(head input), 
+                PotentialSteps [Step(A, Distance(head input))]
+            ),
+            Path (ShortestStep(A, B), Distance (head input + head (tail (tail input))), 
+                PotentialSteps [Step(A, Distance(head input)), Step(B, Distance(head (tail (tail input))))]
+            ),
+            Path (ShortestStep(B, A), Distance(head (tail input) + head (tail (tail input))), 
+                PotentialSteps [Step(B, Distance(head (tail input))), Step(C, Distance(head (tail (tail input))))]
+            ),
+            Path (ShortestStep(B, B), Distance(head (tail input)), 
+                PotentialSteps [Step(B, Distance(head (tail input)))]
+            )
+        ]
+    | otherwise = 
+        QuadPaths [
+            Path (ShortestStep(A, A), Distance(head input), 
+                PotentialSteps [Step(A, Distance(head input))]
+            ),
+            Path (ShortestStep(A, B), Distance(head input), 
+                PotentialSteps [Step(A, Distance(head input)), Step(C, Distance 0)]
+            ),
+            Path (ShortestStep(B, A), Distance(head (tail input)), 
+                PotentialSteps [Step(B, Distance(head (tail input))), Step(C, Distance 0)]
+            ),
+            Path (ShortestStep(B, B), Distance(head (tail input)), 
+                PotentialSteps [Step(B, Distance(head (tail input)))]
+            )
+        ]
 
 -- main execution of program
 main :: IO()
 main = 
-    print("Calling fibTopDownRec: " ++ show (optimalPaths [50, 10, 30, 5, 90, 20, 40, 2, 25, 10, 8]))
+    -- print("Calling fibTopDownRec: " ++ show (optimalPaths [50, 10, 30, 5, 90, 20, 40, 2, 25, 10, 8]))
+    print("Calling qpList [1, 2, 3]: \n" ++ show (qpList [50, 10, 30, 5, 90, 20, 40, 2, 25, 10, 8]))
